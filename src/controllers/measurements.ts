e10b7de614f26f2c4e4fd21115bf0e16dc118169
marc-berends <berendsmarcanthony@gmail.com>
2025-01-10 23:36:17 +0100

    handle City not found case

--- src/controllers/measurements.ts
+++ src/controllers/measurements.ts
@@ -2,17 +2,24 @@ import { RequestHandler } from 'express';
 import { getCache, getCityCache, getLargerCache, getSmallerCache } from '../cache/cache';
 
 export const temperatures: RequestHandler = async (req, res, next) => {
-	res.json(getCache());
+	res.status(200).json(getCache());
 };
 
 export const cityTemperatures: RequestHandler = async (req, res, next) => {
-	res.json(getCityCache(req.params.city));
+	const city = getCityCache(req.params.city);
+	
+	if (!city) {
+		res.status(404).json({ message: 'City not found' });
+		return;
+	}
+
+	res.status(200).json(city);
 };
 
 export const cityTemperatureLarger: RequestHandler = async (req, res, next) => {
-	res.json(getLargerCache(parseFloat(req.params.temp)));
+	res.status(200).json(getLargerCache(parseFloat(req.params.temp)));
 };
 
 export const cityTemperatureSmaller: RequestHandler = async (req, res, next) => {
-	res.json(getSmallerCache(parseFloat(req.params.temp)));
+	res.status(200).json(getSmallerCache(parseFloat(req.params.temp)));
 };
